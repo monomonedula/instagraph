2cafbd4a805d8a0d3393b5fff65475b93aee6f81
monomonedula <valh@tuta.io>
2019-11-19 22:47:30 +0200

    posts info

--- instagraph/gathering/followers.py
+++ instagraph/gathering/followers.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from time import sleep
+
+from abc_delegation import delegation_metaclass
 
 from ..persistence.actions import Actions
 from ..persistence.users import User
@@ -33,6 +34,10 @@ class InstaUser(ABC):
         pass
 
     @abstractmethod
+    def save_posts_info(self):
+        pass
+
+    @abstractmethod
     def retrieve_followers(self):
         pass
 
@@ -41,44 +46,36 @@ class InstaUser(ABC):
         pass
 
 
-class LazyInstaUser(InstaUser):
+class LazyInstaUser(InstaUser, metaclass=delegation_metaclass("_user")):
     def __init__(self, insta_user: InstaUser, actions: Actions):
         self._user = insta_user
         self._actions = actions
 
-    def id(self) -> int:
-        return self._user.id()
-
     def save_followers(self):
-        if not self._actions.followers_explored(self._user.id()):
+        if not self._actions.followers_explored(self.id()):
             self._user.save_followers()
-            self._actions.mark_followers_explored(self._user.id())
+            self._actions.mark_followers_explored(self.id())
 
     def save_following(self):
-        if not self._actions.following_explored(self._user.id()):
+        if not self._actions.following_explored(self.id()):
             self._user.save_following()
-            self._actions.mark_following_explored(self._user.id())
-
-    def retrieve_followers(self):
-        return self._user.retrieve_followers()
-
-    def retrieve_following(self):
-        return self._user.retrieve_following()
+            self._actions.mark_following_explored(self.id())
 
     def save_info(self):
-        if not self._actions.info_saved(self._user.id()):
-            sleep(5)
+        if not self._actions.info_saved(self.id()):
             self._user.save_info()
-            self._actions.mark_info_saved(self._user.id())
+            self._actions.mark_info_saved(self.id())
+
+    def save_posts_info(self):
+        if not self._actions.posts_info_saved(self.id()):
+            self._user.save_posts_info()
+            self._actions.mark_posts_info_saved(self.id())
 
 
-class RecursiveInstaUser(InstaUser):
+class RecursiveInstaUser(InstaUser, metaclass=delegation_metaclass("_target")):
     def __init__(self, user: InstaUser, actions):
         self._target = user
         self._actions = actions
-
-    def id(self):
-        return self._target.id()
 
     def save_followers(self):
         self._target.save_followers()
@@ -90,17 +87,16 @@ class RecursiveInstaUser(InstaUser):
         for f in self._target.retrieve_followers():
             LazyInstaUser(f, self._actions).save_following()
 
-    def retrieve_followers(self):
-        return self._target.retrieve_followers()
-
-    def retrieve_following(self):
-        return self._target.retrieve_following()
-
     def save_info(self):
         self._target.save_info()
         self._target.save_followers()
         for f in self._target.retrieve_followers():
             LazyInstaUser(f, self._actions).save_info()
+
+    def save_posts_info(self):
+        self._target.save_posts_info()
+        for f in self._target.retrieve_followers():
+            LazyInstaUser(f, self._actions).save_posts_info()
 
 
 class SimpleInstaUser(InstaUser):
@@ -112,7 +108,7 @@ class SimpleInstaUser(InstaUser):
         self._following = None
 
     def id(self):
-        return int(self._pg_user.id())
+        return self._pg_user.id()
 
     def retrieve_followers(self):
         if self._followers is None:
@@ -147,3 +143,7 @@ class SimpleInstaUser(InstaUser):
             bio=info["biography"],
             category=info.get("category"),
         )
+
+    def save_posts_info(self):
+        self._pg_user.info()
+
