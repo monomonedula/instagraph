6a142622309f8a7ec7d3d35000599d7331d761bd
monomonedula <valh@tuta.io>
2019-11-20 20:31:34 +0200

    pglocation implemented

--- instagraph/persistence/users.py
+++ instagraph/persistence/users.py
@@ -397,27 +397,49 @@ class PgUserInfo(UserInfo):
 
 
 class PgLocations(Locations):
-    # TODO: implement this
+    def __init__(self, pgsql):
+        self._pgsql = pgsql
+
     def location(self, pk):
-        pass
+        self._pgsql.exec(
+            "INSERT INTO locations (id) VALUES %s "
+            "ON CONFLICT DO NOTHING",
+            [pk]
+        )
+        return PgLocation(self._pgsql, pk)
 
 
 class PgLocation(Location):
-    #TODO: implement this
+    def __init__(self, pgsql, location_id):
+        self._pgsql = pgsql
+        self._id = location_id
+
     def id(self):
-        pass
+        return self._id
 
     def update_lat_lng(self, lat: float, lng: float) -> None:
-        pass
+        self._pgsql.exec(
+            "UPDATE locations SET lat = %s, lng = %s WHERE id = %s",
+            [lat, lng, self._id]
+        )
 
     def update_name(self, name: str) -> None:
-        pass
+        self._pgsql.exec(
+            "UPDATE locations SET name = %s WHERE id = %s",
+            [name, self._id]
+        )
 
     def name(self) -> str:
-        pass
+        return self._pgsql.exec(
+            "SELECT name FROM locations WHERE id = %s",
+            [self._id]
+        )[0].name
 
     def lat_lng(self) -> Tuple[float, float]:
-        pass
+        return self._pgsql.exec(
+            "SELECT lat, lng FROM locations WHERE id = %s",
+            [self._id]
+        )[0]
 
 
 class PgUserMedia(UserMedia):
