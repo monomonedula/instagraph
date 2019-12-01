e4d45a5fad13bdaa64341b43ee86306e6bb772f7
monomonedula <valh@tuta.io>
2019-12-01 15:15:03 +0200

    refactor

--- instagraph/persistence/pg/user.py
+++ instagraph/persistence/pg/user.py
@@ -1,22 +1,24 @@
-from instagraph.persistence.interfaces import User, AlreadyFollowing, UserInfo, UserMedia
+from instagraph.persistence.interfaces import User, AlreadyFollowing, UserInfo, UserMedia, Users
+from instagraph.persistence.pg.pgsql import PgsqlBase
 from instagraph.persistence.pg.user_media import PgUserMedia
 from instagraph.persistence.pg.user_info import PgUserInfo
 from instagraph.persistence.pg.connections import PgUserFollowing, PgUserFollowers
 
 
 class PgUser(User):
-    def __init__(self, pgsql, id_):
+    def __init__(self, pgsql: PgsqlBase, id_: int, users: Users):
         self._pgsql = pgsql
         self._id = id_
+        self._users = users
 
     def id(self):
         return self._id
 
     def following(self):
-        return PgUserFollowing(self._pgsql, self._id)
+        return PgUserFollowing(self._pgsql, self._id, self._users)
 
     def followers(self):
-        return PgUserFollowers(self._pgsql, self._id)
+        return PgUserFollowers(self._pgsql, self._id, self._users)
 
     def schedule_follow(self, user: User, tags=tuple(), priority=5):
         if self.following().is_following(user):
